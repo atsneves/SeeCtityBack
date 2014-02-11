cb5aebb69c199e8852ce7d4dffa036a04d3a9af7
Anderson Neves <atsneves@gmail.com>
2014-02-11 18:04:06 -0200

    Adicionado arquivos

---
+++ application.js
@@ -0,0 +1,574 @@
+var express = require('express')
+  , partials = require('express-partials')
+  , app = express();
+var request = require('needle');
+var database = require("./model/database.js");
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var mime = require('mime');
+var validator = require('validator');
+var datejs = require('safe_datejs');
+var tLog = database.LogErro;
+var DbDevice = database.Device;
+
+
+var DbCrime = database.Crime;
+var mongoose = require("mongoose");
+
+var cadastroModule = require("./model/administrator.js");
+var DbCadastro = database.Cadastro;
+
+
+
+app.configure(function(){
+	app.set("views","views");
+	app.use(partials());
+	app.use(express.favicon());
+	app.use(express.logger());
+	app.use(express.cookieParser());
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use('/', express.static('public'));
+	app.use(express.session({secret: 'sessionLoginHomeEae'}));
+	app.engine('.html', require('ejs').renderFile);
+	app.set("view engine","html");
+	app.set("view options",{
+		layout: 'home'
+	});
+	
+});
+
+app.post("/generateLogin",function(req,res,next){
+	require('crypto').randomBytes(2, function(ex, buf) {
+		var tokenBuf = buf.toString('hex');
+		var token = new Object();
+		token.name = "seecity"+tokenBuf;
+		
+		res.json(token);
+		
+		console.log(token);
+	});
+	
+});
+
+app.get("/",function(req,res,next){
+	if(req.session.admin)
+	{
+		res.redirect("/home");
+	}
+	else
+	{
+		res.render('index',{layout: 'login',title:"Aprovador",erro:req.session.erro,code:""});
+		req.session.destroy();
+		
+	}
+	
+	
+});
+app.get("/userNew/:login/:senha/:tipo/:situacao/:nome",function(req,res,next){
+	
+	console.log(req.params);
+	request.post("http://127.0.0.1:8080/createAdmin",{login:req.params.login,senha:req.params.senha,tipo:req.params.tipo,nome:req.params.nome,situacao:"ATIVO"},function(error, response, body){
+		console.log("erro é:"+error);
+		//console.log(response);
+		
+	});
+	res.end();
+});
+app.post("/novousuario", function(req,res,next){
+	console.log(req.body);
+	
+	
+	if(req.body.identificador)
+	{
+		
+		
+	}
+	else
+	{
+		require('crypto').randomBytes(20, function(ex, buf) {
+			var tokenBuf = buf.toString('hex');
+			
+			var cadastro = new DbCadastro();
+			
+			cadastro.nome = req.body.nome;
+			cadastro.usuario = req.body.login;
+			cadastro.senha = req.body.senha;
+			cadastro.cidade = req.body.cidade;
+			cadastro.email = req.body.email;
+			cadastro.token = tokenBuf;
+			cadastro.tipo = "USER";
+			cadastro.situacao = "ATIVO";
+			cadastro.save(function(err,salvo){
+				
+				if(!err)
+				{
+					res.json(salvo);
+				}
+				else
+				{
+					res.json(err);
+				}
+					
+			});
+			
+		});
+	}	
+	
+
+
+	
+});
+
+
+app.post("/login",function(req,res,next){
+	console.log(req.body);
+	if(req.body.isFacebook == "true")
+	{
+		
+		DbCadastro.findOne({email : req.body.email}, function(err, retDatabase) {
+	        if (retDatabase) 
+	        {
+				res.json(retDatabase)
+	        }	
+	        else
+	        {
+	        	res.json(err);
+	        }
+	    });
+	}
+	else
+	{
+		console.log("entrou no nao face");
+		
+		if(validator.isEmail(req.body.login))
+		{
+			DbCadastro.findOne({email : req.body.login,senha:req.body.senha}, function(err, retDatabase) {
+		        console.log(req.body);
+		        if (retDatabase) 
+		        {
+					res.json(retDatabase)
+		        }	
+		        else
+		        {
+		        	res.json(err);
+		        }
+		    });
+		}
+		else
+		{
+			DbCadastro.findOne({usuario : req.body.login,senha:req.body.senha}, function(err, retDatabase) {
+		        console.log(req.body);
+		        if (retDatabase) 
+		        {
+					res.json(retDatabase)
+		        }	
+		        else
+		        {
+		        	res.json(err);
+		        }
+		    });
+		}
+		
+		
+		
+	}	
+	
+});
+
+
+
+app.post("/",function(req,res,next){
+	
+	DbCadastro.findOne({usuario : req.body.login,senha:req.body.senha}, function(err, retDatabase) {
+        console.log(req.body);
+        if (retDatabase) 
+        {
+			console.log(retDatabase);
+			
+			req.session.admin = retDatabase;
+			
+			res.redirect("/home");
+        }	
+        else
+        {
+        	req.session.erro = {code:500,message:"Usu&aacute;rio ou senha inv&aacute;lidos"};
+        	res.redirect("/");
+        }
+    });
+});
+
+
+app.get("/logout",function(req,res,next){
+	req.session.destroy();
+	res.redirect("/");
+});
+
+app.get("/admin/form",function(req,res,next){
+	
+	if(!req.session.admin)
+	{	
+		req.session.erro = {code:400,message:"Sem acesso"};
+		res.redirect("/");
+		return;
+	}
+	console.log(req.query);
+	
+	
+	
+	if(req.query.p1)
+	{
+		DbCadastro.findOne({_id:req.query.p1},function(err,ret){
+			DbEmpresa.find().sort().exec(function (err, listAllEmpresa) {
+				
+				res.render('formAdmin',{layout: 'home',qryEmpresa:listAllEmpresa,param:ret,title:"Aprovador",username:req.session.admin.nome,code:err});
+				
+			});
+		});
+	}
+	else
+	{
+		DbEmpresa.find().sort().exec(function (err, listAllEmpresa) {	
+			
+			res.render('formAdmin',{layout: 'home',title:"Aprovador",qryEmpresa:listAllEmpresa,username:req.session.admin.nome,code:err});
+			
+		});
+		
+		
+	}
+	
+	
+});
+
+
+app.get("/admin/list",cadastroModule.findAll);
+
+app.get("/admin/listUser",cadastroModule.findAllUser);
+
+app.post("/createAdmin",function(req,res,next){
+//	if(!req.session.admin)
+//	{	
+//		req.session.erro = {code:400,message:"Sem acesso"};
+//		res.redirect("/");
+//		return;
+//	}
+	
+	if(req.body.identificador)
+	{
+		DbCadastro.findOne({_id:req.body.identificador},function(err,ret){
+			ret.usuario = req.body.login;
+			ret.senha = req.body.senha;
+			ret.nome = req.body.nome;
+			ret.situacao = req.body.situacao;
+			ret.cod_usuario = req.body.cod_usuario;
+			if(req.body.situacao == "INATIVO")
+			{
+				var today = new Date();
+				var unsafeToday = today.AsDateJs(); 
+				ret.data_termino= unsafeToday;
+			}
+			
+			ret.save(function(err,saveAdmin){
+				console.log(err);
+				console.log(saveAdmin);
+				if(!err)
+				{
+					req.session.erro = {code:666,message:"Usuário alterado com sucesso!"};
+					res.redirect("/home");
+				}
+				else
+				{
+					req.session.erro = {code:666,message:err.message};
+					res.redirect("/home");
+				}
+				
+			});
+		});
+		
+	}
+	else
+	{
+		var admin = new DbCadastro();
+		admin.usuario = req.body.login;
+		admin.senha = req.body.senha;
+		admin.tipo = req.body.tipo;
+		admin.nome = req.body.nome;
+		admin.situacao = req.body.situacao;
+		admin.cod_usuario = req.body.cod_usuario;
+		var today = new Date();
+		var unsafeToday = today.AsDateJs(); 
+		admin.data_inicio = unsafeToday;
+		
+		
+		admin.id_empresa = req.body.empresa;
+		admin.save(function(err,saveAdmin){
+			console.log(err);
+			if(!err)
+			{
+				req.session.erro = {code:666,message:"Usuário incluído com sucesso!"};
+				res.redirect("/home");
+			}
+			else
+			{
+				req.session.erro = {code:666,message:err.message};
+				res.redirect("/home");
+			}
+		});
+	}
+});
+app.get("/home",function(req,res,next){
+	console.log(req.session.admin);
+	
+	if(req.session.admin)
+	{
+		console.log(req.session.admin);
+		err = "";
+	
+		res.render('index',{layout: 'home',username:req.session.admin.nome,title:"Aprovador",erro:req.session.erro});
+	}
+	else
+	{
+		req.session.erro = {code:400,message:"Sem acesso"};
+		res.redirect("/");
+	}
+});
+
+
+
+app.post("/registerDevice",function(req,res,next){
+	if(!req.session.admin)
+	{
+		req.session.erro = {code:400,message:"Sem acesso"};
+		res.redirect("/");
+	}
+	
+	if(req.body.udid)
+	{
+		exists = false;
+		DbDevice.findOne({udid:{$ne : req.body.udid}},function(error,retorno)
+		{
+			device = new DbDevice();
+			device.udid = req.body.udid;
+			device.device = req.body.device;
+			device.version = req.body.ios;
+
+			device.save(function(err,saveXml){
+				
+				if(!err)
+				{
+					res.json(saveXml);
+				}
+				else
+				{
+					res.json(err);
+				}
+			});
+		});
+		
+		///Caso não tenha nenhum device;
+		device = new DbDevice();
+		device.udid = req.body.udid;
+		device.device = req.body.device;
+		device.version = req.body.ios;
+
+		device.save(function(err,saveXml){
+			
+			if(!err)
+			{
+				res.json(saveXml);
+			}
+			else
+			{
+				res.json(err);
+			}
+		});
+		
+				
+	}
+	else
+	{
+		erro.code = 666;
+		erro.message = "Todos os campos são obrigatórios.";
+		res.json(erro);
+	}
+	
+});
+
+
+app.get("/sendpush",function(req,res,next){
+//	
+//	
+//	var id = req.query.p1;
+//	
+//	
+//	DbXml.findOne({_id:id},function(err,retRev){
+//		console.log(retRev);
+//		
+//		DbDevice.find().exec(function(error,retorno){
+//			console.log("DbDevice = "+retorno);
+//			for (i = 0;i< retorno.length;i++)
+//			{
+//				console.log("retorno titulo = "+retorno[i].udid);
+//				var 
+//				  cert_and_key = require('fs').readFileSync('./apns-dev.pem')
+//				  notifier = require('node_apns').services.Notifier({ cert: cert_and_key, key: cert_and_key }, true /* development = true, production = false */)
+//
+//
+//				 /* 
+//				   Now you may send notifications!
+//				 */
+//
+//				 var Notification = require('node_apns').Notification;
+//
+//				 notifier.notify(Notification("55c62665bad08c74a91a5208667e7ea89acaf69e747209699dc9ce0d3fccb831", { aps: { alert:"Nova Edição: "+p1, sound: "default" }}), 
+//				   function (err) { 
+//					 if (!err)
+//						{
+//							console.log("Sent", this);
+//							res.json(this);
+//						}  
+//						else 
+//						{
+//							console.log('Error', err, 'on', this);
+//							res.json(err);
+//						}
+//				   }
+//				 );
+//
+//				
+//			}
+//			
+//			
+//			
+//		});
+//				
+//	});
+//	
+	
+	
+});
+
+//Create LOG
+
+function createLog(ip,mensagem,tipo)
+{
+	var log = new LogErro();
+	
+	log.ip = ip;
+	log.mensagem = mensagem;
+	log.tipo = tipo;
+	
+	log.save(function(err,saveXML){
+		if(!err)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+			
+	});
+
+}
+
+
+app.post("/removeAdmin",function(req,res,next){
+	var retApp = req.query.app;
+	
+	if(!req.session.admin && !retApp )
+	{
+		req.session.erro = {code:400,message:"Sem acesso"};
+		res.redirect("/");
+	}
+	if(req.body.id)
+	{
+		DbCadastro.remove( {"_id": req.body.id},function(err,ret){
+			if(!err)
+			{
+				req.session.erro = {code:001,message:"Cadastro removido com sucesso!"};
+			}
+			else
+			{
+				req.session.erro = err;
+			}
+			res.end();
+		});
+		
+		
+	}
+});
+
+
+app.post("/addCrime",function(req,res,next){
+	
+	if(req.body.categoria)
+	{
+		var crime = new DbCrime();
+		crime.categoria = req.body.categoria;
+		crime.descricao = req.body.descricao;
+		crime.localizacao = [req.body.longitude,req.body.latitude];
+		crime.usuario = req.body.usuario;
+		crime.save(function(err,salvo){
+			
+			if(!err)
+			{
+				res.json(salvo);
+			}
+			else
+			{
+				res.json(err);
+			}
+		});
+		
+	}
+	
+});
+
+app.post("/listaCrime",function(req,res,next){
+	
+	var pCenterLatitude = req.body.latitude;
+	var pCenterLongitude = req.body.longitude;
+	var pRadiusInKM = 5;
+	var EARTH_RADIUS = 6371;
+	var geoQuery = {
+
+			localizacao: { 
+
+			$nearSphere: [pCenterLongitude, pCenterLatitude],
+
+			$maxDistance: pRadiusInKM/EARTH_RADIUS
+
+			}
+
+	};
+	
+	DbCrime.find(geoQuery).sort({data:-1}).exec(function (err, listaCrime) {
+		if(!err)
+		{
+			res.json(listaCrime);
+		}
+		else
+		{
+			res.json(err);
+		}
+	});
+	
+});
+
+app.configure("development",function(){
+	app.use(express.errorHandler({dumpExceptions:true,showStack:true}));
+	app.set("db-uri","mongodb://localhost/aprovador");
+	
+});
+
+app.configure("production",function(){
+	app.use(express.errorHandler());
+	app.set("db-uri","mongodb://localhost/aprovador");
+});
+
+app.db = mongoose.createConnection(app.set("db-uri"));
+
+app.listen(8080);
---
+++ model/database.js
@@ -0,0 +1,65 @@
+var databaseURL = ("mongodb://localhost/aprovador");
+var mongoose = require("mongoose");
+mongoose.connect(databaseURL);
+Schema = mongoose.Schema;
+
+///Cria a tabela de administrador
+
+var TABLE_CADASTRO = "Cadastro";
+var TABLE_DEVICE = "Device";
+var TABLE_LOG_EROO = "Erro";
+var TABLE_CRIME = "Crime";
+
+var enumTipo = ["ADMIN_GERAL", "USER"];
+var enumSituacao = ["INATIVO", "ATIVO"];
+
+var enumCrime = ["FURTO","ASSALTO","ASSALTOCURSO","ALARME","ATIVIDADE","ACIDENTE","ATENTADOPUDOR","DROGAS","ARROMBAMENTO","SEQUESTRORELAMPAGO","ARRASTAO"];
+
+var enumTipoLog = ["ERRO", "NOERRO"];
+
+var CadastroSchema = new Schema({
+	nome:{type:String,required:true},
+	usuario: {type:String,required:true,unique: true},
+	email: {type:String,required:true,unique: true},
+	senha: {type:String,required:true},
+	tipo: {type: String, required: true, enum: enumTipo},
+	situacao: {type: String, required: true, enum: enumSituacao,default:"INATIVO"},
+	token_ativacao:{type:String},
+	cidade:{type:String},
+	data_inicio: {type: Date,default: Date.now}
+});
+
+var DeviceSchema = new Schema({
+	udid: {type: String, required:true,unique: true},
+	device: {type:String, required:true},
+	version:{type:String, required:true},
+	cadastro: {type:String, required:true}
+});
+
+var CrimeSchema = new Schema ({
+	categoria: {type: String, required: true, enum: enumCrime,default:"FURTO"},
+	descricao: {type: String},
+	localizacao: {type: [Number], index: "2d"},
+	usuario:{type: String, required: true},
+	data: {type: Date,default: Date.now}
+});
+
+var LogErroSchema = new Schema ({
+	ip:{type:String,required:true},
+	mensagem: {type:String,required:true},
+	tipo: {type: String, required: true, enum: enumTipoLog,default:"NOERRO"},
+	data: {type: Date,default: Date.now}
+});
+
+mongoose.model(TABLE_LOG_EROO, LogErroSchema);
+exports.LogErro = mongoose.model(TABLE_LOG_EROO);
+
+
+mongoose.model(TABLE_CADASTRO, CadastroSchema);
+exports.Cadastro = mongoose.model(TABLE_CADASTRO);
+
+mongoose.model(TABLE_DEVICE, DeviceSchema);
+exports.Device = mongoose.model(TABLE_DEVICE);
+
+mongoose.model(TABLE_CRIME, CrimeSchema);
+exports.Crime = mongoose.model(TABLE_CRIME);
