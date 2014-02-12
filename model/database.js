e6967391469a773528fa63513303fcbfa44e19b5
Anderson Neves <atsneves@gmail.com>
2014-02-12 02:35:19 -0200

    Erro database

--- model/database.js
+++ model/database.js
@@ -51,7 +51,7 @@ var CrimeSchema = new Schema ({
 var ComentarioSchema = new Schema ({
 	usuario:{type: String, required: true},
 	data: {type: Date,default: Date.now},
-	comentario: {type: String, required:true}
+	comentario: {type: String, required:true},
 	crime: {type: String, required:true}
 });
 
